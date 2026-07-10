3d8ed4d8027a75fc61b36e371f70555cc537f458
長島清華 <165996410+c0c2404768@users.noreply.github.com>
2026-07-10 17:58:27 +0900

    Revert "Revert "ホーム画面修正""

--- apps/app.py
+++ apps/app.py
@@ -28,7 +28,10 @@ st.markdown(
 )
 
 if "favorites" not in st.session_state:
-    st.session_state.favorites = []
+
+    st.session_state.favorites=[]
+
+
 
 if "quiz_answered" not in st.session_state:
     st.session_state.quiz_answered = False
